ed5acbed1703bf5ea7c8efc96f28e59a784f46b5
2Deo <ziemowit.holda@gmail.com>
2017-10-08 16:17:23 +0200

    Create song model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+song1 = Song.create!( { name: "One More Kiss", album: "From Amsterdam with Love", artist: "Johnny Delafontaine", year: 2049} )
+song2 = Song.create!( { name: "Cigarettes and Coffee", album: "Bleeding with your tears", artist: "Lady Vaga", year: 2023} )
+song3 = Song.create!( { name: "My Little Pony", album: "Pony Massacre Deluxe", artist: "Mary Jane", year: 2032} )
