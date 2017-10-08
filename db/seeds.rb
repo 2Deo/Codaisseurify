c6f8463b3150f3718a01494c4e5b73fda185696b
2Deo <ziemowit.holda@gmail.com>
2017-10-08 16:49:25 +0200

    Create artist model

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,7 @@
 song1 = Song.create!( { name: "One More Kiss", album: "From Amsterdam with Love", artist: "Johnny Delafontaine", year: 2049} )
 song2 = Song.create!( { name: "Cigarettes and Coffee", album: "Bleeding with your tears", artist: "Lady Vaga", year: 2023} )
 song3 = Song.create!( { name: "My Little Pony", album: "Pony Massacre Deluxe", artist: "Mary Jane", year: 2032} )
+
+artist1 = Artist.create!( { name: "Johnny Delafontaine", song: "One More Kiss", image_url: "http://www.20min.ch/dyim/9abc18/M600,1000/images/content/1/0/6/10620881/5/topelement.jpg"} )
+artist2 = Artist.create!( { name: "Lady Vaga", song: "Cigarettes and Coffee", image_url: "https://i.ytimg.com/vi/DIzeeGpwkVM/hqdefault.jpg"} )
+artist3 = Artist.create!( { name: "Mary Jane", song: "My Little Pony", image_url: "https://c1.staticflickr.com/3/2858/13394294324_c6bf58b949_b.jpg"} )
