ade77a188836e24ba8d84f4c509d44a39d8e6e11
2Deo <ziemowit.holda@gmail.com>
2017-10-14 14:43:19 +0200

    Adding routing to new song

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -5,7 +5,10 @@ class ArtistsController < ApplicationController
 
   def show
     @artist = Artist.find(params[:id])
-
+  end
+  
+  def new
+    @song = Song.new
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   root "artists#index"
 
   get "artists" => "artists#index"
+  get "songs/new" => "songs#new",as: :new_song
   get "artists/:id" => "artists#show", as: :artist
 
+  get "songs" => "artists#index"
 
 end
