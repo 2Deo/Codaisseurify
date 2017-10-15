751a9d192589e7d032e5ba9a418e3c331ba2cd43
2Deo <ziemowit.holda@gmail.com>
2017-10-15 19:27:40 +0200

    Controller and routes update

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,5 +1,35 @@
 class SongsController < ApplicationController
+  before_action :set_song, only: [:show]
+
+  def index
+    @songs = current_artist.songs
+  end
+
+  def show; end
+
   def new
-    @song = Song.new
+    @song = current_artist.songs.build
+  end
+
+  def create
+    @song = current_artist.songs.build(song_params)
+
+    if @song.save
+      redirect_to @song, notice: "Song created"
+    else
+      render :new
+    end
+  end
+
+
+
+  private
+
+  def set_song
+    @song = Song.find(params[:id])
+  end
+
+  def song_params
+    params.require(:song).permit(:name)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root "artists#index"
 
+    # resources :artists, only: [:show]
+    # resources :songs
+
   get "artists" => "artists#index"
   get "songs/new" => "songs#new",as: :new_song
   get "artists/:id" => "artists#show", as: :artist
